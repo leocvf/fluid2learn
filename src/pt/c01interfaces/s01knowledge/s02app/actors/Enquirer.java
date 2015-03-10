afc5aeb167695e42838d86ae99821546af1c109b
leo2leo <sleo1010@hotmail.com>
2015-03-10 16:20:25 -0300

    Update Enquirer.java

--- src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
+++ src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
@@ -11,8 +11,9 @@ import pt.c01interfaces.s01knowledge.s01base.inter.IResponder;
 public class Enquirer implements IEnquirer
 {	
 	private static IBaseConhecimento bc = new BaseConhecimento();
+	/* Criamos um vetor de Strings com os nomes dos animais */
 	private static String[] animals = bc.listaNomes();
-    IObjetoConhecimento[] obj = new IObjetoConhecimento[animals.length];
+    	IObjetoConhecimento[] obj = new IObjetoConhecimento[animals.length];
     
 	public Enquirer()
 	{
@@ -29,42 +30,49 @@ public class Enquirer implements IEnquirer
 	public void connect(IResponder responder)
 	{
 		boolean flag, flag2;
+		/* Criamos um vetor de declaracoes para armazenar as perguntas feitas e suas respostas */
 		IDeclaracao[] decl = new Declaracao[40];
+		/* Variavel auxiliar de declaracoes */
 		IDeclaracao declaux;
 		int k = 0;
 		int i = 0;
 		declaux = obj[i].primeira();
-		for(i = 0; i < animals.length && declaux != null; i++)//percorre cada animal
+		for(i = 0; i < animals.length && declaux != null; i++)//percorre cada animal perguntando
 		 {
 		 	flag = true;
-				for(declaux = obj[i].primeira(); declaux != null && flag;  declaux = obj[i].proxima())
+		 	/* Perecorre cada pergunta ate a resposta for diferente da esperada */
+			for(declaux = obj[i].primeira(); declaux != null && flag;  declaux = obj[i].proxima())
+			{
+				flag2 = true;
+				/* Olha se ja perguntou para nao repetir */
+				for(int j = 0; j < k && flag && flag2; j++)
 				{
-					flag2 = true;
-					for(int j = 0; j < k && flag && flag2; j++)
+					/* Se for a mesma pergunta */
+					if(declaux.getPropriedade().equalsIgnoreCase(decl[j].getPropriedade()))
 					{
-						if(declaux.getPropriedade().equalsIgnoreCase(decl[j].getPropriedade()))
+						flag2 = false;
+						/* Se for reposta diferente */
+						if(!declaux.getValor().equalsIgnoreCase(decl[j].getValor()))
 						{
-							flag2 = false;
-							if(!declaux.getValor().equalsIgnoreCase(decl[j].getValor()))
-							{
-								flag = false;
-								
-							}
-	
-						}
-					}
-					if(flag2)
-					{
-						decl[k] = new Declaracao(declaux.getPropriedade(), responder.ask(declaux.getPropriedade()));
-						if(!declaux.getValor().equalsIgnoreCase(decl[k].getValor()))
-						{
-							
 							flag = false;
+								
 						}
-						k++;
-						
+	
 					}
 				}
+				if(flag2)
+				{
+					/* Poe uma nova pergunta ao vetor das repetidas*/
+					decl[k] = new Declaracao(declaux.getPropriedade(), responder.ask(declaux.getPropriedade()));
+					if(!declaux.getValor().equalsIgnoreCase(decl[k].getValor()))
+					{
+							
+						flag = false;
+					}
+					k++;
+						
+				}
+			}
 		}
 		boolean acertei = false;
 		
